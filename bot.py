bccd30d7e4725ce65293cfde37be45ee98fa3115
Lee Scholefield <leescholefield@gmail.com>
2017-05-23 22:17:40 +0100

    Submissions are now added to a dictionary

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ Creates a new Reddit connection and then polls each subreddit in the config.yml 
 import yaml
 import praw
 
-# contains login details for creating a new Reddit instance
+# contains login details for creating a new Reddit instance and a list of subreddits to poll
 with open('config.yml', 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
@@ -12,7 +12,12 @@ reddit = praw.Reddit(client_id=cfg['oauth']['client_id'],
                      client_secret=cfg['oauth']['client_secret'],
                      user_agent=cfg['oauth']['user_agent'])
 
+submissions = {}
 for sub in cfg['subreddits']:
+    submissions[sub] = []
     for submission in reddit.subreddit(sub).new(limit=10):
-        print(submission.title)
-    print('\n')
+        submissions[sub].append(submission)
+
+for subreddit, subs in submissions.items():
+    print(subreddit)
+    print(subs)
