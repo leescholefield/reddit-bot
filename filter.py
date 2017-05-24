f820f11ad546a6cbd446458493edf3406a1d09e8
Lee Scholefield <leescholefield@gmail.com>
2017-05-24 20:29:56 +0100

    Added filter utility method

--- filter.py
+++ filter.py
@@ -5,6 +5,28 @@ from datetime import datetime
 import re
 
 
+def filter_by(submissions, *args, **kwargs):
+    """
+    Utility function. Filters the submissions list by the methods provided in args.
+
+    :param submissions: submission list to filter
+    :param args: methods to filter it by. Either 'date', 'domain' or 'self-posts'
+    :param kwargs: additional data required for desired filtering methods.
+    :return: a list of filtered submissions.
+    """
+    for val in args:
+        if val == 'self-posts':
+            submissions = filter_selfposts(submissions)
+        elif val == 'domain':
+            submissions = filter_by_domain(submissions, kwargs['domain'])
+        elif val == 'date':
+            submissions = filter_by_date(submissions, kwargs['date'])
+        else:
+            raise ValueError(val, ' is not recognized as a valid param')
+
+    return submissions
+
+
 def filter_by_date(submissions, date):
     """
     Removes the submissions from the list made before the given date.
